62e062998f24eaacfbe745f2d480ce0406da7dbc
mzieba <ziebik@gmail.com>
2014-09-14 12:54:12 +0200

    - uproszczone wykonywanie wygenerowanego zapytania insert

--- src/ZPP/Aura/SqlQuery/Insert.php
+++ src/ZPP/Aura/SqlQuery/Insert.php
@@ -4,10 +4,16 @@ namespace ZPP\Aura\SqlQuery;
 
 class Insert extends \Aura\SqlQuery\Mysql\Insert
 {
+    // używając traits
     use \ZPP\Aura\SqlQuery\PdoTrait;
 
     public function execute() {
+        // nadpisujemy kolumny w insercie
         $this->cols(array_keys($this->getBindValues()));
-        return $this->pdo->perform($this->__toString(), $this->getBindValues());
+        
+        // wykonanie zapytania
+        $this->pdo->perform($this->__toString(), $this->getBindValues());
+
+        return $this->pdo->lastInsertId();
     }
 }
