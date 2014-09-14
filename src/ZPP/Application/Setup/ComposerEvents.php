a680a9aaae3b4652aeb1ce74c2d75d0f514c03e2
mzieba <ziebik@gmail.com>
2014-09-14 20:43:08 +0200

    - przeniesienie pliku

---
+++ src/ZPP/Application/Setup/ComposerEvents.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace ZPP\Application\Setup;
+
+class ComposerEvents
+{
+    public static function postInstall(\Composer\Script\CommandEvent $event) {
+        $ds = DIRECTORY_SEPARATOR;
+        $ext = '';
+
+        if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
+            $ext = '.bat';
+        }
+        
+        print `vendor{$ds}bin{$ds}bowerphp{$ext} install`;
+    }
+
+    public static function postUpdate(\Composer\Script\CommandEvent $event) {
+        $ds = DIRECTORY_SEPARATOR;
+        $ext = '';
+
+        if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
+            $ext = '.bat';
+        }
+        
+        print `vendor{$ds}bin{$ds}bowerphp{$ext} update`;
+    }
+}
