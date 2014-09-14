ce569bbdb7998495b2ab9277a34354292b6a0cf6
mzieba <ziebik@gmail.com>
2014-09-14 20:46:40 +0200

    - ujednolicenie kodu

--- public/index.php
+++ public/index.php
@@ -17,31 +17,5 @@ define('DEBUG', 'local' === getenv('APP_ENV'));
 // dołączamy definicję autoloadera
 include 'vendor/autoload.php';
 
-// raportowanie błędów
-error_reporting(E_ALL & ~E_NOTICE & ~E_DEPRECATED);
-
 // tworzymy obiekt naszej aplikacji
-$app = new \Slim\Slim([
-    'templates.path' => 'src/views',
-    'debug' => true
-]);
-$app->view(new \ZPP\View\SlimExtended());
-
-// konfigurację logów
-include 'config/bootstrap.logs.php';
-
-// sesje i bd
-include 'config/bootstrap.db.php';
-
-// routing
-$routes = new \ZPP\Router\Routes;
-$routes->fromArray(include 'config/routes.php');
-$routes->install($app);
-
-// przykład użycia
-// pełny sposób: $monolog = $app->container->get('syslog');
-DEBUG && $app->syslog->addInfo('odpalenie aplikacji', [
-    'ip' => $_SERVER['REMOTE_ADDR'],
-]);
-
-$app->run();
+\ZPP\Application::start();
